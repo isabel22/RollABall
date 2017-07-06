15deccdc3503bba6c7b64ab110eaa690dcd48be0
Isabel <isabel@isabels-mbp.localdomain>
2017-07-06 15:48:41 +0200

    First commit for the Roll a Ball game already working. The restart button is not working by the moment.

---
+++ Assets/Scripts/ClickController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickController : MonoBehaviour {
+
+	private UnityEngine.UI.Button reload;
+
+	// Use this for initialization
+	void Start () {
+		reload = GameObject.Find("Reload").GetComponent<UnityEngine.UI.Button>();
+		disableButton();
+		
+	}
+	
+	// Update is called once per frame
+	public void Click1 () {
+		restart ();
+	}
+
+	void disableButton() 
+	{
+		reload.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+
+	public void restart(){
+		reload.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int count;
+	private UnityEngine.UI.Button reload;
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		reload = GameObject.Find("Reload").GetComponent<UnityEngine.UI.Button>();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.tag = "Picked Up";
+			other.transform.localScale = new Vector3(0.0f,0.0f,0.0f);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 11) {
+			winText.text = "You won";
+			enableButton ();
+		}
+	}
+
+	void enableButton() 
+	{
+		reload.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+	}
+
+	public void Restart()
+	{
+		winText.text = "";
+		count = 0;
+		setCountText ();
+		rb.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		GameObject[] collinders = GameObject.FindGameObjectsWithTag ("Picked Up");
+		foreach (GameObject collinder in collinders) {
+			collinder.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+			collinder.gameObject.tag = "Pick Up";
+		}
+	}
+}
